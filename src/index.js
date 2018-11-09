2f4f22455d7685cf2d29e376ae24c72bffa4562b
Jascha <jascha@jaeh.at>
2018-11-09 01:52:53 +0100

    better error handling, webroot creation

--- src/index.js
+++ src/index.js
@@ -9,14 +9,20 @@ const render = util.promisify(pug.render)
 const PUG = async ({ buffer, config }) => {
   const { ENV } = config
   const HTML_DIR = config.HTML_DIR ? config.HTML_DIR : '/'
-  const WEB_ROOT = ENV !== 'development' && config.WEB_ROOT ? config.WEB_ROOT : '/'
+  const WEB_ROOT = ENV === 'production' && config.WEB_ROOT ? config.WEB_ROOT : '/'
 
   try {
-    if (!buffer || typeof buffer !== 'string') {
+    if (!buffer) {
       throw new Error('PUG: Missing argument: { buffer } missing in first argument')
     }
 
-    const html = await render(buffer, { basedir: HTML_DIR, WEB_ROOT, ...config })
+    if (typeof buffer !== 'string' && typeof buffer.toString === 'function') {
+      buffer = buffer.toString()
+    } else {
+      throw new Error('PUG: buffer has to be a string or be an object with a .toString function')
+    }
+
+    const html = await render(buffer, { basedir: HTML_DIR, ...config, WEB_ROOT })
     const minified = await minify(html)
     return minified
   }
