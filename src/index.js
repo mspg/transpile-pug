20e173c343524d5ac7636469e1c24823435f7393
Jascha <jascha@jaeh.at>
2018-11-09 14:19:28 +0100

    use magic types, refactor, fix bugs found through tests

--- src/index.js
+++ src/index.js
@@ -2,27 +2,35 @@ const util = require('util')
 const pug = require('pug')
 const { minify } = require('html-minifier')
 
+const is = require('@magic/types')
 const log = require('@magic/log')
 
 const render = util.promisify(pug.render)
 
 const PUG = async ({ buffer, config }) => {
   const { ENV } = config
-  const HTML_DIR = config.HTML_DIR ? config.HTML_DIR : '/'
-  const WEB_ROOT = ENV === 'production' && config.WEB_ROOT ? config.WEB_ROOT : '/'
+  config.HTML_DIR = config.HTML_DIR
+    ? config.HTML_DIR
+    : '/'
+
+  config.WEB_ROOT = ENV === 'production' && config.WEB_ROOT
+    ? config.WEB_ROOT
+    : '/'
 
   try {
-    if (!buffer) {
+    if (is.empty(buffer)) {
       throw new Error('PUG: Missing argument: { buffer } missing in first argument')
     }
 
-    if (typeof buffer !== 'string' && typeof buffer.toString === 'function') {
-      buffer = buffer.toString()
-    } else {
-      throw new Error('PUG: buffer has to be a string or be an object with a .toString function')
+    if (!is.string(buffer)) {
+      if (is.buffer(buffer)) {
+        buffer = buffer.toString()
+      } else {
+        throw new Error('PUG: buffer has to be a string or buffer')
+      }
     }
 
-    const html = await render(buffer, { basedir: HTML_DIR, ...config, WEB_ROOT })
+    const html = await render(buffer, { basedir: config.HTML_DIR, ...config })
     const minified = await minify(html)
     return minified
   }
